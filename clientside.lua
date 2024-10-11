9bed6539d2d5a3460586376cae3b70bc3cfb8a1f
lama <114886575+lama-crox@users.noreply.github.com>
2024-10-12 00:27:14 +0200

    Add files via upload
    You need those files for it to work.

---
+++ clientside.lua
@@ -0,0 +1,35 @@
+local requestLib = {waitThreshold = 1.25}
+
+-- THE ARGUMENTS SHOULD BE WRITTEN IN A PYTHON FORMAT!
+
+function requestLib:httpPost(url: 'https://example.com', headers: 'Header Input', json: 'Json Input', inputfile, outputfile)
+    if not inputfile or not outputfile then return end
+    writefile(outputfile, '')
+    writefile(inputfile, string.format('POST:Cut:%s:Cut:Cut:%s:Cut:%s', url, headers, json))
+
+    local timeSince, content = tick()
+    while true do
+        if tick() - timeSince > requestLib.waitThreshold then break end
+        content = readfile(outputfile)
+        if content ~= '' then break end
+    end
+
+    return content
+end
+
+function requestLib:httpGet(url: 'https://example.com', params: {'Params'}, inputfile, outputfile)
+    if not inputfile or not outputfile then return end
+    writefile(outputfile, '')
+    writefile(inputfile, string.format('GET:Cut:%s:Cut:Cut:%s:Cut:%s', url, params))
+
+    local timeSince, content = tick()
+    while true do
+        if tick() - timeSince > requestLib.waitThreshold then break end
+        content = readfile(outputfile)
+        if content ~= '' then break end
+    end
+
+    return content
+end
+
+return requestLib
---
+++ serverside.py
@@ -0,0 +1,34 @@
+from requests import post, get
+from time import sleep
+from pathlib import Path
+from json import loads
+
+configuration = {'checkRate': 0.1}
+data = {'inputfile': 'path/to/input/file', 'outputfile': 'path/to/output/file'}
+
+def readFile():
+    if Path.exists(data['inputfile']):
+        with open(data['inputfile'], 'r') as file:
+            return file.read()
+
+def writeFile(data):
+    if Path.exists(data['outputfile']):
+        with open(data['outputfile'], 'w') as file:
+            return file.write(data)
+
+while True:
+    try:
+        fileData = readFile()
+        if fileData == '': continue
+
+        parameters = fileData.split(':Cut:')
+        if parameters[0] == 'POST':
+            response = post(parameters[1], headers=loads(parameters[2]), json=loads(parameters[3]))
+            writeFile(response)
+        elif parameters[0] == 'GET':
+            response = get(parameters[1], params=loads(parameters[2]))
+            writeFile(response)
+    except Exception as e:
+        writeFile(f'Failed to fetch data: {str(e)}')
+
+    sleep(configuration['checkRate'])
